7ff13e258710375f10b437f97accf90b03a604f8
Marc Sauer <marc@sauer.systems>
2021-03-31 22:01:27 +0200

    System: Added new Controller for handling flight filing Style: Added a new view, this time with bootstrap
    With this I will prepare the switch over to bootstrap. I like tailwind,
    but my experience is too less with it.

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FlightController;
 
 Route::get('/auth/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/auth/register', [RegisterController::class, 'store']);
@@ -18,9 +19,9 @@ Route::post('/auth/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/user/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/flights', function () {
-    return view('flights.index');
-});
+Route::get('/user/flights/add', [FlightController::class, 'index'])->name('addflight');
+
+
 
 Route::get('/', function () {
     return view('home.index');
