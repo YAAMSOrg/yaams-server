be12234accdb75d4b1b7ec15cd2268d86069e77b
Marc Sauer <m.sauer@khm.de>
2021-03-31 15:36:44 +0200

    Auth: Added migration for setting a user active status'

---
+++ resources/views/airlines/airlinelist.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="flex justify-center">
+            <div class="w-8/12 bg-white p-6 rounded-lg">
+                <h1 class="text-4xl font-normal leading-normal mt-0 mb-2 text-black-800">
+                    All current airlines
+                </h1>
+                <p>
+                    Content
+                </p>
+            </div>
+    </div>
+
+
+@endsection
--- resources/views/flights/add.blade.php
+++ resources/views/flights/add.blade.php
@@ -5,7 +5,7 @@
     <div class="flex justify-center">
             <div class="w-8/12 bg-white p-6 rounded-lg">
                 <h1 class="text-4xl font-normal leading-normal mt-0 mb-2 text-black-800">
-                    Flights
+                    File flight
                 </h1>
                 <p>
                     Content
