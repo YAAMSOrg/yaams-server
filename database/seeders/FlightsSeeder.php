0a454976c36b27615662cc9b866134d7d27e48db
Marc Sauer <info@marcsauer.net>
2024-04-09 10:35:57 +0200

    Added FlightsSeeder for example flights.

---
+++ database/seeders/FlightsSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+
+class FlightsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('flights')->insert([
+            'airline' => "1",
+            'callsign' => "1337",
+            'flightnumber' => "3446",
+            'departure_icao' => "EDDK",
+            'arrival_icao' => 'EDDS',
+            'aircraft' => '1',
+            'crzalt' => '21000',
+            'blockoff' => '2024-04-09 06:24:26.000000',
+            'blockon' => '2024-04-09 07:15:35.000000',
+            'burned_fuel' => '3185',
+            'route' => 'KUMIK Y854 BOMBI T721 SUNEG T715 KRH T128 BADSO',
+            'online_network' => '3',
+            'pilot' => '2',
+            'remarks' => 'Example remark',
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString(),
+        ]);
+    }
+}
