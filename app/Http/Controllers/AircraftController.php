35f448a94c02c098600d8819bad82fe033cdde40
Marc Sauer <info@marcsauer.net>
2024-04-18 11:40:19 +0200

    Fixed owner checking of the aircraft

--- app/Http/Controllers/AircraftController.php
+++ app/Http/Controllers/AircraftController.php
@@ -6,6 +6,7 @@ use App\Models\Aircraft;
 use App\Models\Airport;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Validation\ValidationException;
 
 class AircraftController extends Controller
@@ -70,67 +71,76 @@ class AircraftController extends Controller
         $currentActiveAirline = $request->session()->get('activeairline');
 
         //Check if users airline owns the aircraft
-        if(!$currentActiveAirline->id = $aircraft->airline->id) {
+        if($currentActiveAirline->id !== $aircraft->airline->id) {
             return redirect()->route('dashboard')->with('error', 'You did something nasty!');
-        }
-
-        /**
-         * Since we get "on" or "off" from the form checkbox element, we need to convert this into usable values. If we get "on", we can set $gotStatus to true.
-         */
-        $gotStatus = $request->post('active');
-        if($gotStatus == "on"){
-            $finalStatus = true;
         } else {
-            $finalStatus = false;
-        }
-
-        // Check if the request is a POST
-        if($request->getMethod() == "POST"){
-            // Validate input
-            // TODO: Is this possible to define the validation rule up above and just use it here? It's nearly the same anyway...
-            $validated = $request->validate([
-                'registration' => 'required|uppercase|max:6',
-                'manufacturer' => 'required',
-                'model' => 'required',
-                'remarks' => 'nullable',
-            ]);
-
-            // Actually change the values
-            $targetAircraft = Aircraft::find($aircraft->id);
-            $targetAircraft->registration = $request->post('registration');
-            $targetAircraft->used_by = $currentActiveAirline->id;
-            $targetAircraft->manufacturer = $request->post('manufacturer');
-            $targetAircraft->model = $request->post('model');
-            $targetAircraft->active = $finalStatus;
-            $targetAircraft->remarks = $request->post('remarks');
-
-            // If we notice, that the registration has changed, we need to make a check if there is an aircraft with the same tail number already active, since we can't have the same tail number active twice.
-            if ($targetAircraft->isDirty('registration') || $targetAircraft->isDirty('active')) {
-
-                $existingAircraft = Aircraft::where('registration', $request->post('registration'))
-                    ->where('used_by', $currentActiveAirline->id)
-                    ->where('id', '<>', $aircraft->id) // Exclude current aircraft
-                    ->where('active', true)
-                    ->exists();
-
-                if ($existingAircraft) {
-                    throw ValidationException::withMessages(['registration' => 'An active aircraft with this tail number already exists in this airline. Please set the aircraft inactive or choose another tail number.']);
-                }
+            /**
+             * Since we get "on" or "off" from the form checkbox element, we need to convert this into usable values. If we get "on", we can set $gotStatus to true.
+             */
+            $gotStatus = $request->post('active');
+            if($gotStatus == "on"){
+                $finalStatus = true;
+            } else {
+                $finalStatus = false;
             }
-            // Save the changes.
-            $targetAircraft->save();
 
-            // And boom: Back to fleetmanager :)
-            return redirect()->route('fleetmanager');
+            // Check if the request is a POST
+            if($request->getMethod() == "POST"){
+                // Validate input
+                // TODO: Is this possible to define the validation rule up above and just use it here? It's nearly the same anyway...
+                $validated = $request->validate([
+                    'registration' => 'required|uppercase|max:6',
+                    'manufacturer' => 'required',
+                    'model' => 'required',
+                    'remarks' => 'nullable',
+                ]);
+
+                // Actually change the values
+                $targetAircraft = Aircraft::find($aircraft->id);
+                $targetAircraft->registration = $request->post('registration');
+                $targetAircraft->used_by = $currentActiveAirline->id;
+                $targetAircraft->manufacturer = $request->post('manufacturer');
+                $targetAircraft->model = $request->post('model');
+                $targetAircraft->active = $finalStatus;
+                $targetAircraft->remarks = $request->post('remarks');
+
+                // If we notice, that the registration has changed, we need to make a check if there is an aircraft with the same tail number already active, since we can't have the same tail number active twice.
+                if ($targetAircraft->isDirty('registration') || $targetAircraft->isDirty('active')) {
+
+                    $existingAircraft = Aircraft::where('registration', $request->post('registration'))
+                        ->where('used_by', $currentActiveAirline->id)
+                        ->where('id', '<>', $aircraft->id) // Exclude current aircraft
+                        ->where('active', true)
+                        ->exists();
+
+                    if ($existingAircraft) {
+                        throw ValidationException::withMessages(['registration' => 'An active aircraft with this tail number already exists in this airline. Please set the aircraft inactive or choose another tail number.']);
+                    }
+                }
+                // Save the changes.
+                $targetAircraft->save();
+
+                // And boom: Back to fleetmanager :)
+                return redirect()->route('fleetmanager');
+            }
+
+            // If we just get a GET request, display the manager.
+            return view('fleet.edit', ['aircraft' => $aircraft ]);
         }
-
-        // If we just get a GET request, display the manager.
-        return view('fleet.edit', ['aircraft' => $aircraft ]);
     }
 
     public function view(Aircraft $aircraft) {
-        // TODO: Check if user is member of the aircrafts owner airline.
-        return view('fleet.detail', ['aircraft' => $aircraft ]);
+        $currentActiveAirline = Session::get('activeairline');
+
+        dump($currentActiveAirline->id);
+        dump($aircraft->airline->id);
+
+        //Check if users airline owns the aircraft
+        if($currentActiveAirline->id !== $aircraft->airline->id) {
+            return redirect()->route('dashboard')->with('error', 'You did something nasty!');
+        } else {
+            return view('fleet.detail', ['aircraft' => $aircraft ]);
+        }
     }
 }
 
